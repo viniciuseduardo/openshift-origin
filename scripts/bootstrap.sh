e03007e73c76416a711b2c897b195cf465c20383
Vinicius Alves <vinicius.alves@sysmi.com.br>
2018-07-11 13:40:05 -0300

    Update Scripts Deploy

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -6,6 +6,6 @@ SUBSCRIPTION_ID=$2
 RESOURCE_GROUP_NAME="sysmi-$PROJECT_NAME-rg"
 DEPLOYMENT_NAME="sysmi-$PROJECT_NAME-deploy"
 
-echo "Creating Deployment: Openshift on Azure "
-az group deployment create --name $DEPLOYMENT_NAME --resource-group $RESOURCE_GROUP_NAME --subscription $SUBSCRIPTION_ID --template-file azuredeploy.json --parameters @azuredeploy.parameters.json --no-wait
-echo "Created Deployment: Openshift on Azure "
+echo "Creating Deployment: $DEPLOYMENT_NAME"
+az group deployment create --name $DEPLOYMENT_NAME --resource-group $RESOURCE_GROUP_NAME --subscription $SUBSCRIPTION_ID --template-file azuredeploy.json --parameters @azuredeploy.parameters.markel.json --no-wait
+echo "Created Deployment: $DEPLOYMENT_NAME"
